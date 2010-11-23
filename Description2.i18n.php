0347ff19c710c1c274e88e334ae86a731cd7bb9a
Raimond Spekking <raymond@users.mediawiki.org>
2010-11-23 20:20:45 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-11-22 20:11:00 UTC)

--- Description2.i18n.php
+++ Description2.i18n.php
@@ -15,3 +15,24 @@ $messages['en'] = array(
 	'description2-desc' => 'Adds a description meta-tag to MediaWiki pages and into the ParserOutput for other extensions to use',
 );
 
+/** Breton (Brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'description2-desc' => 'Ouzhpennañ a ra un deskrivadur meta-tikedennet da bajennoù MediaWiki hag er Parser Output da vezañ implijet gant astennoù all',
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'description2-desc' => 'Fügt dem Seitenquelltext das Meta-Element „description“ hinzu sowie zusätzlich zur Nutzung durch andere Softwareerweiterungen der Parserausgabe',
+);
+
+/** Upper Sorbian (Hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'description2-desc' => 'Přidawa metatafličku "description" do stronow MediaWiki a do ParserOutput za wužiwanje druhich rozšěrjenjow',
+);
+
