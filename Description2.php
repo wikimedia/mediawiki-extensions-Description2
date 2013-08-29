ea7963db3a94df4166cae6294e1c8c17c0ae1ee9
Daniel Friesen <daniel@nadir-seen-fire.com>
2013-08-29 02:10:04 -0700

    Update extension metadata. - License blurb - Author lines - Other attributes
    Change-Id: Ib9f62b0e98eb40d41f5ef875f1201f276ee3c50b

--- Description2.php
+++ Description2.php
@@ -1,26 +1,13 @@
 <?php
 /**
- * Description2.php -- Adds meaningful description <meta> tag to MW pages and into the parser output
- * Copyright 2010 Daniel Friesen
+ * Description2.php – Adds meaningful description <meta> tag to MW pages and into the parser output
  *
  * @file
  * @ingroup Extensions
- * @author Daniel Friesen (http://mediawiki.org/wiki/User:Dantman) <mediawiki@danielfriesen.name>
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * @author Daniel Friesen (http://danf.ca/mw/)
+ * @copyright Copyright 2010 – Daniel Friesen
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @link https://www.mediawiki.org/wiki/Extension:Description2 Documentation
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) die( "This is an extension to the MediaWiki package and cannot be run standalone." );
@@ -29,7 +16,7 @@ $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Description2',
 	'version' => '0.2',
-	'author' => 'Daniel Friesen',
+	'author' => "[http://danf.ca/mw/ Daniel Friesen]",
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Description2',
 	'descriptionmsg' => 'description2-desc',
 );
