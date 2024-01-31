055927a901bf538df11b6a04f30812bdaaa6bad7
xtex <xtexchooser@duck.com>
2024-01-31 16:13:34 +0800

    Remove style tags in description
    Some pages' description may include CSS code rendered by [[Extension:TemplateStyles]].
    
    Change-Id: I352ac2338eb5977305308546523ec6c55f7cb599

--- includes/SimpleDescriptionProvider.php
+++ includes/SimpleDescriptionProvider.php
@@ -8,7 +8,7 @@ class SimpleDescriptionProvider implements DescriptionProvider {
 	 * Extracts description from the HTML representation of a page.
 	 *
 	 * The algorithm:
-	 * 1. Removes all <table> elements and their contents.
+	 * 1. Removes all <style> and <table> elements and their contents.
 	 * 2. Selects all <p> elements.
 	 * 3. Iterates over those paragraphs, strips out all HTML tags and trims white-space around.
 	 * 4. Then the first non-empty paragraph is picked as the result.
@@ -17,8 +17,12 @@ class SimpleDescriptionProvider implements DescriptionProvider {
 	 * @return string
 	 */
 	public function derive( string $text ): ?string {
-		$pattern = '%<table\b[^>]*+>(?:(?R)|[^<]*+(?:(?!</?table\b)<[^<]*+)*+)*+</table>%i';
-		$myText = preg_replace( $pattern, '', $text );
+		$myText = $text;
+		$stripTags = [ 'style', 'table' ];
+		foreach ( $stripTags as $tag ) {
+			$pattern = "%<$tag\b[^>]*+>(?:(?R)|[^<]*+(?:(?!</?$tag\b)<[^<]*+)*+)*+</$tag>%i";
+			$myText = preg_replace( $pattern, '', $myText );
+		}
 
 		$paragraphs = [];
 		if ( preg_match_all( '#<p>.*?</p>#is', $myText, $paragraphs ) ) {
